b6581f05449ac640ff7231833713deae1d9e19f9
Peter van Hardenberg <pvh@heroku.com>
2011-04-30 23:42:04 -0700

    rewrite parsing logic to be halfway credible

--- monkey.rb
+++ monkey.rb
@@ -1,20 +1,60 @@
 # timing is bad
+require 'strscan'
 
 class Sequel::Postgres::HStore < Hash
+  def self.parse_quotable_string(scanner)
+    if scanner.scan(/"/)
+      key = scanner.scan(/(\\"|[^"])+/).gsub("\\", "")
+      scanner.skip(/"/)
+      key
+    else
+      value = scanner.scan(/\w+/)
+      value = nil if value == "NULL"
+      # TODO: values but not keys may be NULL
+    end
+  end
+
+  def self.skip_key_value_delimiter(scanner)
+    scanner.skip(/\s*=>\s*/) 
+  end
+
+  def self.skip_pair_delimiter(scanner)
+    scanner.skip(/,\s*/)
+  end
+
   def self.new_from_string(string)
     hash = {}
-    string.split(/,\s*/).each do |pair|
-      (key, value) = pair.split(/\s*=>\s*/)
-      # strip the quotes off
-      hash[key[1..-2]] = value[1..-2]
+    scanner = StringScanner.new(string)
+    while !scanner.eos?
+      key = parse_quotable_string(scanner)
+      skip_key_value_delimiter(scanner)
+      value = parse_quotable_string(scanner)
+      skip_pair_delimiter(scanner)
+
+      hash[key] = value
     end
+
     self[hash]
   end
   def initialize(hash)
     @hash = hash
   end
+
   def sql_literal(dataset)
-    "\'{" + self.map { |(k,v)| "#{k.to_s} => #{v.to_s}" }.join(", ") + "}\'"
+    string = self.map do |(k,v)|
+      if v.nil?
+        # represent nil as NULL without quotes
+        v = "NULL"
+      else
+        # otherwise, write a double-quoted string with backslash escapes for quotes
+        v = v.to_s.gsub('"', '\"')
+        v = "\"#{v}\""
+      end
+      # TODO: throw an error if there is a NULL key
+      "\"#{k.to_s}\" => #{v}"
+    end.join(", ")
+    puts string
+    "'#{string}'"
   end
 end
 
